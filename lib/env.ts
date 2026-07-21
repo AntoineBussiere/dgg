df599f6ad0c5c8bb6e98082ee403f5bb7144ca2f
AntoineBussiere <antoine.bussiere21@gmail.com>
2026-07-21 12:13:30 +0200

    Test env preview

--- lib/env.ts
+++ lib/env.ts
@@ -3,7 +3,7 @@ const env =
     process.env.NODE_ENV ||
     "development";
 
-console.log(env);
+console.log(process.env.VERCEL_ENV);
     
 export const redisPrefix = env === 'production' ? '' : env === 'preview' ? 'PREVIEW-' : 'DEV-';
 
